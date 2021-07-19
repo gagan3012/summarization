10a5b652bcb34f1427d66bf710a52d0b9964fe9f
Gagan Bhatia <gbhatia880@gmail.com>
2021-07-19 15:59:45 -0700

    logging added

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -1,3 +1,5 @@
+import dagshub
+
 from src.models.model import Summarization
 from src.data.make_dataset import make_dataset
 
@@ -9,4 +11,6 @@ def evaluate_model():
     model = Summarization()
     model.load_model()
     results = model.evaluate(test_df=test_df)
+    with dagshub.dagshub_logger() as logger:
+        logger.log_metrics(results)
     return results
