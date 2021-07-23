f9cfbcabd27566789148a72cbe22c3da6777ecac
Gagan Bhatia <gbhatia880@gmail.com>
2021-07-22 21:30:59 -0700

    updates

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
+
 def process_data(split='train'):
-    df= pd.DataFrame()
+    df = pd.DataFrame()
     dataset = pd.load_csv('summarization/data/raw/{}.csv'.format(split))
     df['article'] = dataset['article']
     df['highlights'] = dataset['highlights']
     df.to_csv('summarization/data/processed/{}.csv'.format(split))
+
+
+if __name__ == '__name__':
+    process_data(split='train')
+    process_data(split='test')
+    process_data(split='validation')
