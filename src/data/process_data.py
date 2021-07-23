2466d7fd94a05b06237f49d408515fea11188dc8
Gagan Bhatia <gbhatia880@gmail.com>
2021-07-23 00:29:48 -0700

    updates

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 def process_data(split='train'):
     df = pd.read_csv('C:/Users/gbhat/Documents/GitHub/summarization/data/raw/{}.csv'.format(split))
-    df.rename(columns={"article": "input_text", "highlights": "output_text"})
-    print(df.shape())
+    df.columns = ['Unnamed: 0', 'input_text', 'output_text']
+    print(df.columns)
     df.to_csv('C:/Users/gbhat/Documents/GitHub/summarization/data/processed/{}.csv'.format(split))
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     process_data(split='train')
     process_data(split='test')
     process_data(split='validation')
