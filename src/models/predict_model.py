515949b781fa903367a55729608b54462b7bde6c
Gagan Bhatia <gbhatia880@gmail.com>
2021-07-24 20:04:01 -0700

    added params

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,3 +1,5 @@
+import yaml
+
 from .model import Summarization
 import pandas as pd
 
@@ -6,8 +8,12 @@ def predict_model(text):
     """
     Predict the summary of the given text.
     """
+    with open("params.yml") as f:
+        params = yaml.safe_load(f)
+
+
     model = Summarization()
-    model.load_model()
+    model.load_model(model_type=params['model_type'], model_dir=params['model_dir'])
     pre_summary = model.predict(text)
     return pre_summary
 
