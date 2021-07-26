f01fda57d2b793c19cf7b1f950ff7f8a49a32800
Gagan Bhatia <gbhatia880@gmail.com>
2021-07-26 01:03:32 -0700

    fixes

--- src/models/model.py
+++ src/models/model.py
@@ -479,6 +479,7 @@ class Summarization:
         references = test_df['output_text'][:5]
 
         predictions = [self.predict(x) for x in input_text]
+        print(type(predictions),type(references))
 
         results = metric.compute(predictions=predictions, references=references)
         '''
