5f1d01b90f85a5ff8433fa9312348ea24103f7cf
Gagan Bhatia <gbhatia880@gmail.com>
2021-07-25 22:50:41 -0700

    split added

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -23,8 +23,8 @@ def train_model():
 
     model.train(train_df=train_df, eval_df=eval_df,
                 batch_size=params['batch_size'], max_epochs=params['epochs'],
-                use_gpu=params['use_gpu'], learning_rate=params['learning_rate'],
-                num_workers=params['num_workers'])
+                use_gpu=params['use_gpu'], learning_rate=float(params['learning_rate']),
+                num_workers=int(params['num_workers']))
 
     model.save_model(model_dir=params['model_dir'])
 
