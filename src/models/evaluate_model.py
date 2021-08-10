cbf036a338c41d532cdfb99236c692cbe5e55eed
Gagan Bhatia <gbhatia880@gmail.com>
2021-08-10 15:01:35 -0700

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -17,7 +17,6 @@ def evaluate_model():
     model.load_model(model_type=params["model_type"], model_dir=params["model_dir"])
     results = model.evaluate(test_df=test_df, metrics=params["metric"])
 
-    with dagshub_logger(metrics_path='reports/evaluation_metrics.csv', should_log_hparams=False) as logger:
         logger.log_metrics(results)
 
 
