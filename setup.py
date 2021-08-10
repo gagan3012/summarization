d14733a67b18adc4666009cf38b1704df90330f2
Gagan Bhatia <gbhatia880@gmail.com>
2021-08-10 15:01:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ with open('requirements.txt') as f:
 
 setup(
     name='t5s',
-    packages=find_packages("src"),
     version='0.1.0',
     description="T5 Summarisation Using Pytorch Lightning",
     license='MIT License',
