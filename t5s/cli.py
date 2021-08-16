5c7f8cea8908b299d8d3dcefd7d50dc8c88a1bd2
Gagan Bhatia <gbhatia880@gmail.com>
2021-08-16 16:02:48 -0700

    Update cli.py

--- t5s/cli.py
+++ t5s/cli.py
@@ -47,6 +47,8 @@ parser_start.add_argument(
 )
 parser_start.add_argument(
     "-b", "--batch-size", default=2, help="Enter the number of batches"
+)
+
 parser_dirs = command_subparser.add_parser(
     "dirs",
     help="Create directories that are ignored by git but required for " "the project",
