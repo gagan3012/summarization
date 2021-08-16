198680ccde63d2794a96bc9aec175d7dff10ef92
Gagan Bhatia <gbhatia880@gmail.com>
2021-08-16 16:04:51 -0700

    Update cli.py

--- t5s/cli.py
+++ t5s/cli.py
@@ -140,6 +140,7 @@ def start(arguments):
     dicts.update(newdct)
     dicts.update(data_params)
     print("Final parameters for the run are: {}".format(dicts))
+
 def parse_args(args):
     arguments = vars(arg_parser.parse_args(args=args or ["--help"]))
     return arguments
